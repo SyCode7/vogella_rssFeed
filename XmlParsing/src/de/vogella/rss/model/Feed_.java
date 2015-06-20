24c132689792695047446d44b56c83fb2ce185f3
Kennedy <ktorkura@yahoo.com>
2015-06-20 19:00:22 +0200

    double checking commit

---
+++ XmlParsing/src/de/vogella/rss/model/Feed_.java
@@ -0,0 +1,17 @@
+package de.vogella.rss.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2015-06-20T18:47:15.615+0200")
+@StaticMetamodel(Feed.class)
+public class Feed_ {
+	public static volatile SingularAttribute<Feed, String> title;
+	public static volatile SingularAttribute<Feed, String> link;
+	public static volatile SingularAttribute<Feed, String> description;
+	public static volatile SingularAttribute<Feed, String> language;
+	public static volatile SingularAttribute<Feed, String> copyright;
+	public static volatile SingularAttribute<Feed, String> pubDate;
+	public static volatile SingularAttribute<Feed, Long> id;
+}
---
+++ XmlParsing/src/de/vogella/rss/tests/ReadTest_.java
@@ -0,0 +1,9 @@
+package de.vogella.rss.tests;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2015-06-20T12:11:07.438+0200")
+@StaticMetamodel(ReadTest.class)
+public class ReadTest_ {
+}
